c881442aa77a09c264d63c8713ea54140e8725b7
Aaron Landwehr <snaphat@gmail.com>
2021-04-01 02:50:06 -0400

    - Add album art & artist to OSD info. - Add previous and next track support. - Add Replay track support. - Synchronize Play/Pause state every 5 seconds.

--- pandora_media_session.js
+++ pandora_media_session.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Pandora MediaSession Support
-// @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.2
 // @description  Implements the MediaSession API for Pandora.
 // @author       Aaron Landwehr
 // @match        https://www.pandora.com/*
 // @grant        None
+// @run-at      document-idle
 // ==/UserScript==
 
 (function () {
@@ -19,47 +19,109 @@
     audio.play();
     navigator.mediaSession.playbackState = "playing";
 
-    function simulatePlayPauseClick() {
+
+    window.addEventListener('load', function () { setTimeout(lazyTimer, 500); });
+
+    function lazyTimer() {
+        updateMetadata();
+        setTimeout(lazyTimer, 5000);
+    }
+
+    function updateMetadata() {
+        // Retrieve music information.
+        var title = getText('Tuner__Audio__TrackDetail__title');
+        var artist = getText('Tuner__Audio__TrackDetail__artist');
+        var album = getText('nowPlayingTopInfo__current__albumName');
+        var art = getArt('Tuner__Audio__TrackDetail__img');
+
+        // Get higher quality artwork if possible.
+        if (art) {
+            art = art.replace("90W", "500W").replace("90H", "500H");
+        }
+
+        // Populate metadata.
+        navigator.mediaSession.metadata = new MediaMetadata({
+            title: title,
+            artist: artist,
+            album: album,
+            artwork: [{ src: art, sizes: '128x128', type: 'image/jpeg' }]
+        });
+
+        // Check status of playback and correct media-session info if necessary.
+        var e = document.getElementsByClassName("PlayButton");
+        if (e && e[0]) {
+            if (e[0].getAttribute('data-qa') == "pause_button") {
+                audio.play();
+                navigator.mediaSession.playbackState = "playing";
+            } else {
+                audio.pause();
+                navigator.mediaSession.playbackState = "paused";
+            }
+        }
+
+
+    }
+
+    // Some helper.
+    function getArt(cls) {
+        var e = document.getElementsByClassName(cls);
+        return (e && e[0] && e[0].firstChild && e[0].firstChild.firstChild) ? e[0].firstChild.firstChild.src : "";
+    }
+
+    // Some helper.
+    function getText(cls) {
+        var e = document.getElementsByClassName(cls);
+        return e && e[0] ? e[0].textContent : "";
+    }
+
+    // Simulate clicks because using the JS Pandora object methods and spacebar keys are unreliable.
+    function simulateClick(cls) {
         var clickEvent = new MouseEvent('click', {
             view: null,
             bubbles: true,
             cancelable: true
         });
-        document.getElementsByClassName("PlayButton")[0].dispatchEvent(clickEvent);
+        let e = document.getElementsByClassName(cls)[0];
+        if (e) {
+            e.dispatchEvent(clickEvent);
+        }
     };
 
-    function updateMetadata() {
-        let track = playlist[0];
-
-        console.log('Playing ' + track.title + ' track...');
-        navigator.mediaSession.metadata = new MediaMetadata({
-            title: track.title,
-            artist: track.artist,
-            album: track.album,
-            artwork: track.artwork
-        });
-    }
-
-    /* Play & Pause */
+    // Play
     navigator.mediaSession.setActionHandler('play', async function () {
-        console.log('> User clicked "Play" icon.');
         audio.currentTime = 0;
         audio.play();
-        //Pandora.playTrack();
-        //Pandora.playOrPauseTrack();
-        //document.dispatchEvent(new KeyboardEvent('keydown',{'keyCode':32,'which':32}));
-        simulatePlayPauseClick(); // Simulate clicks because using the JS object and spacebar are unreliable.
+        simulateClick("PlayButton"); // Everywhere button.
+        setTimeout(updateMetadata, 500);
         navigator.mediaSession.playbackState = "playing";
     });
 
+    // Pause
     navigator.mediaSession.setActionHandler('pause', function () {
-        console.log('> User clicked "Pause" icon.');
         audio.currentTime = 0;
         audio.pause();
-        //Pandora.pauseMusic();
-        // Pandora.playOrPauseTrack();
-        //document.dispatchEvent(new KeyboardEvent('keydown',{'keyCode':32,'which':32}));
-        simulatePlayPauseClick(); // Simulate clicks because using the JS object and spacebar are unreliable.
+        simulateClick("PlayButton"); // Everywhere button.
+        setTimeout(updateMetadata, 500);
         navigator.mediaSession.playbackState = "paused";
+    });
+
+    // Prev Track, replay.
+    navigator.mediaSession.setActionHandler('previoustrack', function () {
+        audio.currentTime = 0;
+        audio.play();
+        simulateClick("ReplayButton"); // Station button.
+        simulateClick("Tuner__Control__SkipBack__Button"); // playlist button.
+        setTimeout(updateMetadata, 500);
+        navigator.mediaSession.playbackState = "playing";
+    });
+
+    // Next Track.
+    navigator.mediaSession.setActionHandler('nexttrack', function () {
+        audio.currentTime = 0;
+        audio.play();
+        simulateClick("Tuner__Control__Skip__Button"); // Station button.
+        simulateClick("Tuner__Control__SkipForward__Button"); // playlist button.
+        setTimeout(updateMetadata, 500);
+        navigator.mediaSession.playbackState = "playing";
     });
 })();
