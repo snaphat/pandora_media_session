6e02aa8411b53548bcf82c50d33a24d286f3fa77
Aaron Landwehr <snaphat@gmail.com>
2021-03-31 22:06:21 -0400

    Add missing pause.

--- pandora_media_session.js
+++ pandora_media_session.js
@@ -11,7 +11,8 @@
 (function () {
     'use strict';
 
-    // Add small almost empty audio file so windows since we don't have access to Pandora's actual audio stream directly.
+    // Add small almost empty blank audio file so windows since we don't have access to Pandora's actual audio stream directly.
+    // This is necessary because the OSD won't work without a file (a completely empty file doesn't work either).
     let audio = document.createElement('audio');
     audio.src = "data:audio/x-wav;base64,UklGRooWAABXQVZFZm10IBAAAAABAAEAIlYAAESsAAACABAAZGF0YWYWAAAAAAAAAAAAAAAAAAAAAA";
     audio.loop = true;
@@ -42,19 +43,19 @@
     /* Play & Pause */
     navigator.mediaSession.setActionHandler('play', async function () {
         console.log('> User clicked "Play" icon.');
+        audio.currentTime = 0;
+        audio.play();
         //Pandora.playTrack();
         //Pandora.playOrPauseTrack();
         //document.dispatchEvent(new KeyboardEvent('keydown',{'keyCode':32,'which':32}));
         simulatePlayPauseClick(); // Simulate clicks because using the JS object and spacebar are unreliable.
-        audio.loop = true;
-        audio.currentTime = 0;
-        audio.play();
         navigator.mediaSession.playbackState = "playing";
     });
 
     navigator.mediaSession.setActionHandler('pause', function () {
         console.log('> User clicked "Pause" icon.');
         audio.currentTime = 0;
+        audio.pause();
         //Pandora.pauseMusic();
         // Pandora.playOrPauseTrack();
         //document.dispatchEvent(new KeyboardEvent('keydown',{'keyCode':32,'which':32}));
